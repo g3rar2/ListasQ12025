32628119e0c6f8187295618b7abcca149d7713fb
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-05 18:58:10 -0600

    add listas

--- ListasQ12025/Program.cs
+++ ListasQ12025/Program.cs
@@ -1,1 +1,16 @@
-﻿L
+﻿List<string> listaString=new List<string>();
+
+listaString.Add("Naranja");
+listaString.Add("5");
+listaString.Add("Real España");
+listaString.Add("Barcelona");
+listaString.Add("Real Madrid");
+listaString.Add("Alajuela");
+listaString.Insert(3, "Real Madrid");
+//listaString.Remove("Real Madrid");
+listaString.RemoveAll(lista=> lista.Contains("Real Madrid"));
+
+foreach (string item in listaString)
+{
+    Console.WriteLine(item);
+}
